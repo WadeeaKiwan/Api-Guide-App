cffc9a06d99202593c57ea51911f383b70cd3941
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-01-10 18:58:48 +0100

    prepare to deploy on Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.get(/.*/, (req, res) => {
+  res.sendfile(__direname + '/dist/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started on port: ${PORT}`);
+});
